8a94b0c3927587e99f1f4129127dc4401d339b55
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-03 17:24:35 -0500

    update to run jest on json

--- shape.test.js
+++ shape.test.js
@@ -5,6 +5,7 @@ const {Triangle, Square, Circle} = require("./shape")
 describe('purple Triangle test', () => {
     test("test for a purple triangle", () => {
         const shape = new Triangle(); 
+        shape.setColor("purple")
         expect(shape.render()).toEqual(
             '<polygon points="150, 18 244, 182 56, 182" fill="purple" />'
         );
