8ddc1da8e9ae9f26dbd028b251fd65e194507632
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-03 15:32:15 -0500

    inquirer prompt not working

--- index.js
+++ index.js
@@ -4,3 +4,51 @@ const fs = require("fs");
 
 //adds shapes 
 const {Triangle, Square, Circle} = require("./shape")
+
+// Inquirer prompt to to prompt user to answer q's in command line 
+function promptUser() {
+    inquirer
+      .prompt([
+        // Text prompt
+        {
+          type: "input",
+          message:
+            "What text would you like you logo to display? (Enter up to three characters)",
+          name: "text",
+        },
+        // Text color prompt
+        {
+          type: "input",
+          message:
+            "Choose text color (Enter color keyword OR a hexadecimal number)",
+          name: "textColor",
+        },
+        // Shape choice prompt
+        {
+          type: "list",
+          message: "What shape would you like the logo to render?",
+          choices: ["Triangle", "Square", "Circle"],
+          name: "shape",
+        },
+        // Shape color prompt
+        {
+          type: "input",
+          message:
+            "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+          name: "shapeBackgroundColor",
+        },
+      ])
+      .then((answers) => {
+        // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
+        if (answers.text.length > 3) {
+          console.log("Must enter a value of no more than 3 characters");
+          promptUser();
+        } else {
+          // Calling write file function to generate SVG file
+          writeToFile("logo.svg", answers);
+        }
+      });
+  }
+  
+  // Calling promptUser function so inquirer prompts fire off when application is ran
+  promptUser();
