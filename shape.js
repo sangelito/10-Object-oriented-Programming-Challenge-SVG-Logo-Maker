03d09ca8f74b6c5b2cdbe8ce52065ed289e92c74
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-03 15:06:46 -0500

    square

--- shape.js
+++ shape.js
@@ -16,4 +16,11 @@ class Triangle extends Shape {
       return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
     }
   }
+  // Square class inherits properties defined in Shape class
+class Square extends Shape {
+    render() {
+      // Returns square with color input
+      return `<rect x="73" y="40" width="160" height="160" fill="${this.color}" />`;
+    }
+  }
   
