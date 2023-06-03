98ce9c3bd1aa23fad34b7e1eaa6caf59346c8d71
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-03 17:04:26 -0500

    triangle test

--- shape.test.js
+++ shape.test.js
@@ -2,3 +2,11 @@
 const {Triangle, Square, Circle} = require("./shape")
 
 // case test for purple triangle 
+describe('purple Triangle test', () => {
+    test("test for a purple triangle", () => {
+        const shape = new Triangle(); 
+        expect(shape.render()).toEqual(
+            '<polygon points="150, 18 244, 182 56, 182" fill="purple" />'
+        );
+    });
+});
