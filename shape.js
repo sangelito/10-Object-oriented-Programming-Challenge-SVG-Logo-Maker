0eec2c6877a185b1fcac1fe6118e7d4dc9073461
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-03 15:04:14 -0500

    triangle

--- shape.js
+++ shape.js
@@ -8,4 +8,12 @@ class Shape {
       this.color = colorVar;
     }
   }
+
+  // Triangle class inherits properties defined in Shape class
+class Triangle extends Shape {
+    render() {
+      // Returns triangle with color input
+      return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+    }
+  }
   
