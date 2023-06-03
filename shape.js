32115ce194f55693de313432c1186dddbae5d51d
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-03 14:55:13 -0500

    creating shape class and constructor

---
+++ shape.js
@@ -0,0 +1,11 @@
+// Shape class utilizes constructor to define shape
+class Shape {
+    constructor() {
+      this.color = "";
+    }
+    // set shape color 
+    setColor(colorVar) {
+      this.color = colorVar;
+    }
+  }
+  
